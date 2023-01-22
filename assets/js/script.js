a86b03f0f3dcae85add8da2ac50c1023c6668607
Bethany Ryalls <bethany@motifdesignstudio.com>
2023-01-22 12:54:39 +0000

    creating elements and assigning styled classes to display each work hour, a textarea and save button

--- assets/js/script.js
+++ assets/js/script.js
@@ -31,3 +31,24 @@ var events15 = JSON.parse(localStorage.getItem('threePm')) || "";
 var events16 = JSON.parse(localStorage.getItem('fourPm')) || "";
 var events17 = JSON.parse(localStorage.getItem('fivePm')) || "";
 
+
+
+
+// Displaying all timeblocks
+$.each(hours, function(index, value) {
+    events = [events9, events10, events11, events12, events13, events14, events15, events16, events17];
+
+    // create row div with class row
+    var row = $('<div>').addClass('row');
+    // create div to hold the time with span that holds the hour and AM/PM
+    var timeCol = $('<div>').addClass('hour col-1 text-right pt-3 pr-1').append('<span>' + value.format('hA') + '</span>');
+    // create text area for event column
+    var textArea = $('<textarea>' + events[index] + '</textarea>').addClass('col-12');
+    // create div for event input
+    var eventCol = $('<div>').addClass('col-10 time-block').css({'display':'flex', 'padding-right':'0px'}).append(textArea);
+    // create div for save button
+    var saveBtn = $('<button>').addClass('saveBtn col-1');
+
+    $(row).append(timeCol).append(eventCol).append(saveBtn);
+    $('.container').append(row);
+})
