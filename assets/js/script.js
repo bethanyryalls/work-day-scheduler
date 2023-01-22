6d8b189e1addef9cfaddbf6432d049673d663ba1
Bethany Ryalls <bethany@motifdesignstudio.com>
2023-01-22 10:41:28 +0000

    displaying current day at top of page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,5 @@
-console.log("hey");
+//Get current date using moment
+var currentDay = moment().format('dddd, MMMM Do');
+
+// Display current date at top of page
+$('#currentDay').text(currentDay);
